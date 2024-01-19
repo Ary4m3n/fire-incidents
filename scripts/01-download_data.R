951c0dfff5498a6f754d7ae26c8bbad40f8e5824
Aryaman Sharma <aryaman.sharma@mail.utoronto.ca>
2024-01-19 17:39:53 -0500

    Minor Changes to scripts/01-download_data.R

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -11,10 +11,8 @@
 #### Workspace setup ####
 library(opendatatoronto)
 library(tidyverse)
-# [...UPDATE THIS...]
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
 the_raw_data <-
   read_csv(
     file = "https://ckan0.cf.opendata.inter.prod-toronto.ca/dataset/64a26694-01dc-4ec3-aa87-ad8509604f50/resource/1e824947-d73b-4f48-9bac-7f7f3731a6b9/download/Fire%20Incidents%20Data.csv",
@@ -23,8 +21,6 @@ the_raw_data <-
 
 
 #### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
 write_csv(the_raw_data, "inputs/data/raw_data.csv")
 
          
