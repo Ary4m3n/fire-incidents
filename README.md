beb72e59f3af06211d285fb1706640ba00e19370
Aryaman Sharma <aryaman.sharma@mail.utoronto.ca>
2024-01-22 21:45:15 -0500

    Minor edits to README.md and added comments to 02-data_cleaning.R. Rerendered the paper.

--- README.md
+++ README.md
@@ -6,13 +6,13 @@ Chat-GPT4 was used as an LLM for this paper where the usage was restricted to le
 
 ## Overview
 
-This repo provides all the work and files used and created while writing the paper titled *An Analysis of a Rising Trend in Dollar Loss in Fire Incidents With and Without Sprinkler Systems in Toronto over a decade (2011-2022)*. To access the PDF of the paper you can find it in `outputs/paper` where it is named as `paper.pdf`. @file-structure outlines each directory and also all relevant material that they contain. Please go through the File Structure Section before accessing the repo to gain all the understanding needed.
+This repo provides all the work and files used and created while writing the paper titled *An Analysis of a Rising Trend in Dollar Loss in Fire Incidents With and Without Sprinkler Systems in Toronto over a decade (2011-2022)*. To access the PDF of the paper you can find it in `outputs/paper` where it is named as `paper.pdf`. The File Structure Section outlines each directory and also all relevant material that they contain. Please go through the File Structure Section before accessing the repo to gain all the understanding needed.
 
 To use this folder, click the green "Code" button", then "Download ZIP". The user can then move the folder around to wherever they want to work on it on their own computer.
 
 The repo has been created in a reproducible way for as to allow users to access and reproduce the work easily.
 
-## File Structure {#file-structure}
+## File Structure
 
 The repo has been structured in a way to make it accessible to the user. The repo is structured as:
 
@@ -21,4 +21,4 @@ The repo has been structured in a way to make it accessible to the user. The rep
 -   `input/sketches` contains two sketches for the datasets and the graphs as reference to what is to be expected in the paper.
 -   `outputs/data` contains the cleaned dataset that was constructed.
 -   `outputs/paper` contains the files used to generate the paper, including the Quarto document, `paper.qmd`, the reference bibliography file, `references.bib` as well as the PDF of the paper, `paper.pdf`. 
--   `scripts` contains the R scripts used to simulate, download and clean data. Kindly read the prerequisites in the R scripts' preamble to be able to reuse and run the code.
+-   `scripts` contains the R scripts used to simulate `00-simulate_data.R`, download `01-download_data.R`, clean `02-data_cleaning.R` and test data `03-test_data.R`. Kindly read the prerequisites in the R scripts' preamble to be able to reuse and run the code.
--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -12,66 +12,38 @@ library(tidyverse)
 library(dplyr)
 
 #### Clean data ####
-raw_fire_data <- read_csv("inputs/data/raw_data.csv")
+raw_fire_data <- read_csv("inputs/data/raw_data.csv") # read the csv
 
 cleaned_fire_data <-
-  clean_names(raw_fire_data)|>
-  select(
+  clean_names(raw_fire_data)|> # we clean the names in the raw data
+  select( # we select just the three columns needed
     estimated_dollar_loss,
     tfs_alarm_time,
     sprinkler_system_presence
   ) |>
-  rename(
+  rename( # we rename the columns here
     dollar_loss = estimated_dollar_loss,
     alarm_time = tfs_alarm_time,
     sprinkler_system_presence = sprinkler_system_presence
   ) |>
-  separate(
+  separate( # this step obtains just the year from the alarm_time
     col = alarm_time,
     into = c("year", "Other"),
     sep = "-"
   ) |>
   select(-Other) |>
-  separate(
+  separate( # this step cleans the sprinkler_system_presence column
     col = sprinkler_system_presence,
     into = c("Id", "sprinkler_system_presence"),
     sep = " - "
   ) |>
   select(-Id) |>
-  filter(!is.na(dollar_loss), !is.na(year)) |>
-  rename(
+  filter(!is.na(dollar_loss), !is.na(year)) |> # here we remove any NA fields from the dollar_loss and year columns, this step was referred to from ChatGPT 4 and the chat has been included in usage.txt
+  rename( # here we rename the columns to then be saved in cleaned_data.csv
     "Dollar Loss" = dollar_loss,
     "Year" = year,
     "Sprinkler System Presence" = sprinkler_system_presence
   )
-# the last step of filter was referred to from ChatGPT 4 and the chat has been included in usage.txt
-
-# cleaned_data <-
-#   raw_data |>
-#   janitor::clean_names() |>
-#   select(wing_width_mm, wing_length_mm, flying_time_sec_first_timer) |>
-#   filter(wing_width_mm != "caw") |>
-#   mutate(
-#     flying_time_sec_first_timer = if_else(flying_time_sec_first_timer == "1,35",
-#                                    "1.35",
-#                                    flying_time_sec_first_timer)
-#   ) |>
-#   mutate(wing_width_mm = if_else(wing_width_mm == "490",
-#                                  "49",
-#                                  wing_width_mm)) |>
-#   mutate(wing_width_mm = if_else(wing_width_mm == "6",
-#                                  "60",
-#                                  wing_width_mm)) |>
-#   mutate(
-#     wing_width_mm = as.numeric(wing_width_mm),
-#     wing_length_mm = as.numeric(wing_length_mm),
-#     flying_time_sec_first_timer = as.numeric(flying_time_sec_first_timer)
-#   ) |>
-#   rename(flying_time = flying_time_sec_first_timer,
-#          width = wing_width_mm,
-#          length = wing_length_mm
-#          ) |> 
-#   tidyr::drop_na()
 
 #### Save data ####
 write_csv(cleaned_fire_data, "outputs/data/cleaned_data.csv")
